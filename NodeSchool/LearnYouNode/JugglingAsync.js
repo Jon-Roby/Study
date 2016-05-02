00cbd93c094e535de921f9cd875ea75953c5db5a
Jon Roby <jonathan.roby1@gmail.com>
2016-05-01 17:29:48 -0700

    JugglingAsync

--- NodeSchool/LearnYouNode/JugglingAsync.js
+++ NodeSchool/LearnYouNode/JugglingAsync.js
@@ -29,3 +29,31 @@
 //  rely on a third-party library such as [async](http://npm.im/async) or
 //  [after](http://npm.im/after). But for this exercise, try and do it without
 //  any external helper library.
+
+var http = require('http')
+var bl = require('bl')
+var results = []
+var count = 0
+
+function printResults () {
+  for (var i = 0; i < 3; i++)
+    console.log(results[i])
+}
+
+function httpGet (index) {
+  http.get(process.argv[2 + index], function (response) {
+    response.pipe(bl(function (err, data) {
+      if (err)
+        return console.error(err)
+
+      results[index] = data.toString()
+      count++
+
+      if (count == 3)
+        printResults()
+    }))
+  })
+}
+
+for (var i = 0; i < 3; i++)
+  httpGet(i)
