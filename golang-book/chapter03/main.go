c534954bfa601ad93a1ee1c2bdbd84ebc706f5ad
Jon Roby <jonathan.roby1@gmail.com>
2016-05-21 17:02:04 -0700

    chapter02 and chapter03

---
+++ golang-book/chapter03/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println(len("hello world"))
+  fmt.Println("Hello World"[1])
+  fmt.Println("a"[0])
+  fmt.Println("b"[0])
+  fmt.Println("c"[0])
+  fmt.Println("d"[0])
+  fmt.Println("e"[0])
+  fmt.Println("Hello " + "World")
+}
